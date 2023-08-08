3de13d9bea44807266e753b7c098711963376451
AidenRoh <colohrby@gmail.com>
2023-08-08 10:03:43 +0900

    second

---
+++ aiden/src/main/java/algorithm/aiden/AidenApplication.java
@@ -0,0 +1,13 @@
+package algorithm.aiden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AidenApplication.class, args);
+	}
+
+}
