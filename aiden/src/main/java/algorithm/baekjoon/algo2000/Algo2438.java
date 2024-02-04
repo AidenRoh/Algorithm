5fdeab9a0126ed0ccbabc1bdf3574e8b514da924
AidenRoh <colohrby@gmail.com>
2024-02-04 17:24:53 +0900

    algo2438 change style

--- aiden/src/main/java/algorithm/baekjoon/algo2000/Algo2438.java
+++ aiden/src/main/java/algorithm/baekjoon/algo2000/Algo2438.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Algo2438 {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
         StringBuilder sb1 = new StringBuilder();
         StringBuilder sb2 = new StringBuilder();
